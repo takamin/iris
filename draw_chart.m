cbdd7a45fbfb498c7cd60ab238c3bcc5084dd80b
Koji Takami <vzg03566@gmail.com>
2017-10-19 17:26:07 +0900

    Add draw_chart.m to draw the dataset and errors

---
+++ draw_chart.m
@@ -0,0 +1,35 @@
+# Draw iris charts
+[];
+
+function show_chart(D, error_data, featureX, featureY, chartTitle)
+
+    hold all
+
+    Dy0 = D( D( :, 5 ) == 0, : );
+    scatter(Dy0(:,featureX), Dy0(:,featureY), "r")
+
+    Dy1 = D( D( :, 5 ) == 1, : );
+    scatter(Dy1(:,featureX), Dy1(:,featureY), "g")
+
+    Dy2 = D( D( :, 5 ) == 2, : );
+    scatter(Dy2(:,featureX), Dy2(:,featureY), "b")
+
+    error_data_r = error_data(error_data(:,5)==0, :);
+    scatter(error_data_r(:, featureX), error_data_r(:, featureY), [], "r", "filled")
+
+    error_data_g = error_data(error_data(:,5)==1, :);
+    scatter(error_data_g(:, featureX), error_data_g(:, featureY), [], "g", "filled")
+
+    error_data_b = error_data(error_data(:,5)==2, :);
+    scatter(error_data_b(:, featureX), error_data_b(:, featureY), [], "b", "filled")
+
+    title(chartTitle)
+
+endfunction
+
+subplot(1,2,1)
+show_chart(D, error_data, 1, 2, "X:Sepal length, Y:Sepal width")
+
+subplot(1,2,2)
+show_chart(D, error_data, 3, 4, "X:Petal length, Y:Petal width")
+
